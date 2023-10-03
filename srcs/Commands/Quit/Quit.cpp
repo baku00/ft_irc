e9b296c0d762e9c548a993e82c9244b912b185c4
david-gloriod <137294724+david-gloriod@users.noreply.github.com>
2023-10-04 00:02:03 +0200

    Quit command

---
+++ srcs/Commands/Quit/Quit.cpp
@@ -0,0 +1,16 @@
+#include "Quit.hpp"
+
+Quit::Quit() {
+	this->_minArgsRequired = 0;
+	this->_maxArgsRequired = 0;
+	this->_commandName = "USER";
+}
+
+Quit::~Quit() {}
+
+void	Quit::execute(Client client, std::vector<std::string> args) const {
+	if (!this->isValidArgsNumber(args.size() - 1))
+		return this->errorNumberArguments(client);
+
+	ServerInstance::getInstance()->disconnectClientFromFD(client.getFd());
+}
---
+++ srcs/Commands/Quit/Quit.hpp
@@ -0,0 +1,15 @@
+#ifndef QUIT_HPP
+# define QUIT_HPP
+
+# include <main.hpp>
+# include "../ACommand.hpp"
+# include "../../Client/Client.hpp"
+
+class Quit : public ACommand {
+	public:
+		Quit();
+		virtual ~Quit();
+		virtual void		execute(Client client, std::vector<std::string> args) const;
+};
+
+#endif
