cd5146e7cef33b8b7c1774bae80d6ea8365198ae
baku00 <137294724+baku00@users.noreply.github.com>
2023-11-02 18:09:18 +0100

    For transfert

--- srcs/Server/Server.cpp
+++ srcs/Server/Server.cpp
@@ -180,6 +180,8 @@ void	Server::_disconnectClient(std::vector<pollfd>::iterator it)
 
 	_pollfds.erase(it);
 	std::cout << "Client size: " << _clients.size() << std::endl;
+	for (std::map<int, Client *>::iterator client = this->_clients.begin(); client != this->_clients.end(); client++)
+		std::cout << "fd: " << client->first << "; nickname: " << client->second->getNickname() << std::endl;
 	_clients.erase(it->fd);
 	std::cout << "Client déconnecté" << std::endl;
 }
